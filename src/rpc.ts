49131b67c1ed270f9a81e08727025b17094b4bd9
hntrl <hunter@hntrl.io>
2025-01-04 13:52:27 -0700

    feat(rpc): isRpcError

--- src/rpc.ts
+++ src/rpc.ts
@@ -6,12 +6,28 @@ export class RequestError extends Error {
     super(JSON.stringify(arg));
     this.name = "RequestError";
   }
+  static unmarshal(arg: any) {
+    const reqErr = new RequestError(arg);
+    Object.assign(reqErr, JSON.parse(reqErr.message));
+    return reqErr;
+  }
 }
 
 export function castRpcError(err: Error) {
   const requestErrorParts = err.message.split("RequestError: ");
   if (requestErrorParts.length > 1) {
-    return new RequestError(JSON.parse(requestErrorParts[1]));
+    return RequestError.unmarshal(JSON.parse(requestErrorParts[1]));
   }
   return err;
+}
+
+export function isRpcError(err: Error, eq: Record<string, any>) {
+  err = castRpcError(err as Error);
+  if (err.name === "RequestError") {
+    return Object.entries(eq).every(([key, value]) => {
+      // @ts-expect-error - RequestError properties are intentionally ambiguous
+      return err[key] === value;
+    });
+  }
+  return false;
 }
