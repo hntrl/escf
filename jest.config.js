2eda32b29f5fa69a6ef3300033eacd8980a41f9b
hntrl <hunter@hntrl.io>
2025-01-04 14:17:28 -0700

    chore: add test boiler plate

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
